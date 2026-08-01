87b1dfa4e903061d2a02ec09062673d61d8d91a9
basmalaagomaa7-hub <basmalaagomaa7@gmail.com>
2026-08-01 06:34:43 +0300

    Add files via upload

---
+++ Src/APP/SelfTest/SelfTest_Interface.h
@@ -0,0 +1,24 @@
+/**
+* @file    SelfTest_Interface.h
+* @author (developer)  
+* @author(reviewer)
+* @brief  
+* @details
+* @version
+* @date
+* @copyright Copyright (c) 2026, Gestell Company
+*/
+
+#ifndef SELFTEST_INTERFACE_H
+#define SELFTEST_INTERFACE_H
+
+#include "../../LIB/STD_TYPES.h"
+#include <stdint.h>
+
+#define SELFTEST_PASS    1
+#define SELFTEST_FAIL    0
+
+uint8_t SelfTest_Run();
+
+
+#endif // SELFTEST_INTERFACE_H
