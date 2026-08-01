d99777c2c26fa8cdc71cdf4575995ade86d3441e
basmalaagomaa7-hub <basmalaagomaa7@gmail.com>
2026-08-01 05:17:34 +0300

    Update TIMER_Interface.h

--- Src/MCAL/TIMER/TIMER_Interface.h
+++ Src/MCAL/TIMER/TIMER_Interface.h
@@ -12,6 +12,17 @@
 #ifndef TIMER_INTERFACE_H
 #define TIMER_INTERFACE_H
 
+void TIMER0_Init(Timer0_Config_t Config);
 
+void TIMER0_Start(uint8_t ClockSelectValue);
+
+void TIMER0_Stop(void);
+void TIMER0_SetPreload(uint8_t PreloadValue);
+void TIMER0_SetCompare(uint8_t CompareValue);
+
+void TIMER0_SetCallBackFunction(uint8_t TimerInterruptType, void (*PF)(void));
+
+// comparevalue=2^n*DutyCycle
+void TIMER0_SetPWMDutyCycle(uint8_t DutyCyclePercentage);
 
 #endif // TIMER_INTERFACE_H
