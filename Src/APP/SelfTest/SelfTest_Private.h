641e28ae97bc78a059a2793cdf487bbb38e353ca
basmalaagomaa7-hub <basmalaagomaa7@gmail.com>
2026-08-02 23:34:59 +0300

    Update SelfTest_Private.h

--- Src/APP/SelfTest/SelfTest_Private.h
+++ Src/APP/SelfTest/SelfTest_Private.h
@@ -1,11 +1,11 @@
 /**
 * @file    SelfTest_Private.h
-* @author (developer)  
-* @author(reviewer)
-* @brief  
-* @details
-* @version
-* @date
+* @author  dev Basmala Mahmoud  
+* @author  rev Malak Mohammed
+* @brief   Private declarations for the SelfTest module.
+* @details Contains internal helper function prototypes and private macros used only inside SelfTest_Program.c.
+* @version 1.0
+* @date    8/2/2026
 * @copyright Copyright (c) 2026, Gestell Company
 */
 
