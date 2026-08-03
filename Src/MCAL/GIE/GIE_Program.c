16ea4802e4e43fc1d59b3c3f1002a6187f3a7f7c
basmalaagomaa7-hub <basmalaagomaa7@gmail.com>
2026-08-03 03:29:52 +0300

    Update GIE_Program.c

--- Src/MCAL/GIE/GIE_Program.c
+++ Src/MCAL/GIE/GIE_Program.c
@@ -1,3 +1,14 @@
+/**
+* @file    GIE_Program.c
+* @author  dev CTRLDRIVE TEAM
+* @author  rev Maria Boules 
+* @brief   Implements the GIE driver.
+* @details Provides functions to enable and disable global interrupts by controlling the I-bit in the AVR Status Register (SREG).
+* @version 1.0
+* @date    8/2/2026
+* @copyright Copyright (c) 2026, Gestell Company
+*/
+
 #include "GIE_Interface.h"
 
 
